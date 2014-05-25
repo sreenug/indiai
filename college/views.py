f3b81a81bbc238a223d180adb125076f319f78e6
Aadish Gupta <aadish@digitalgreen.org>
2014-05-25 15:32:09 +0530

    html form

---
+++ college/views.py
@@ -0,0 +1,38 @@
+from django.views.generic import CreateView
+
+from models import Award, College, CollegeForm
+
+from school_data.settings import MEDIA_URL
+from django.http import HttpResponse
+import simplejson
+import json
+import csv
+
+from school_data.settings import PROJECT_PATH
+
+def response_mimetype(request):
+    if "application/json" in request.META['HTTP_ACCEPT']:
+        return "application/json"
+    else:
+        return "text/plain"
+
+class CreateCollegeView(CreateView):
+    model = College
+    template_name = 'college.html'
+    form_class = CollegeForm
+    
+    def form_valid(self, form):
+        self.object = form.save()
+        f = self.request.FILES.get('image')
+        if f:
+            data = [{'name': f.name, 'url': MEDIA_URL + "school/" + f.name.replace(" ", "_")}]
+        response = JSONResponse("Thank You For Filling the Form", {}, response_mimetype(self.request))
+        response['Content-Disposition'] = 'inline; filename=files.json'
+        return response
+
+
+class JSONResponse(HttpResponse):
+    """JSON response class."""
+    def __init__(self,obj='',json_opts={},mimetype="application/json",*args,**kwargs):
+        content = simplejson.dumps(obj,**json_opts)
+        super(JSONResponse,self).__init__(content,mimetype,*args,**kwargs)
--- school_data/urls.py
+++ school_data/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from college.views import CreateCollegeView
 from school.views import CreateSchoolView, excelFile
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', CreateSchoolView.as_view(), name='home'),
+    url(r'^college/$', CreateCollegeView.as_view(), name='college'),
     url(r'^excel/$', excelFile),
     # url(r'^school_data/', include('school_data.foo.urls')),
 
